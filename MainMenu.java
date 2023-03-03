3de932594d9f696817ab760153055d2910ad9023
Cyrah Sophia Angella Cuenca <119916620+i675nrk@users.noreply.github.com>
2023-03-04 06:22:22 +0800

    Add files via upload
    1st test

---
+++ MainMenu.java
@@ -0,0 +1,60 @@
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.Color;
+import javax.swing.WindowConstants;
+
+public class MainMenu extends JFrame implements ActionListener{
+
+    public MainMenu(){
+        main(null);
+    }
+    public static void main(String[] args) {
+
+        ImageIcon icon;
+        icon = new ImageIcon("play button.png");
+        Image i = icon.getImage();
+        icon = new ImageIcon(i.getScaledInstance(135,135, 0));
+
+        ImageIcon imgIcon = new ImageIcon("menubackground.png");
+        ImageIcon bgIcon = new ImageIcon(imgIcon.getImage().getScaledInstance(500, 500, Image.SCALE_SMOOTH));
+        JLabel backgroundLabel = new JLabel(bgIcon);
+        JFrame mainMenu = new JFrame();
+        JButton btnplay = new JButton();
+        btnplay.setBounds(180, 185, 125,95);
+        btnplay.setVisible(true);
+        btnplay.setBackground(Color.BLACK);
+        btnplay.setIcon(icon);
+        mainMenu.add(btnplay);
+        JPanel panel = new JPanel();
+        panel.add(backgroundLabel);
+        mainMenu.add(panel);
+        
+        //Menu Frame
+        mainMenu.setTitle("Tic-Tac-Toe");
+        mainMenu.add(backgroundLabel);
+        mainMenu.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainMenu.setSize(500, 500);
+        mainMenu.setVisible(true);
+        mainMenu.setResizable(false);
+        mainMenu.setLocationRelativeTo(null);
+        
+        
+        
+        btnplay.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){  
+                mainMenu.dispose();
+                new TicTacToe();
+                
+        }  
+    });  
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
